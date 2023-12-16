b31d3967fc54190522032d7b5bf85af0cffc6899
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-17 01:32:20 +0300

    feat: mine-sweeper; 53/83 (01:31:46 Sunday, December 17, 2023)

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let matrixReworked = JSON.parse(JSON.stringify(matrix))
+  for (let i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[0].length; j++) {
+      let counter = 0
+      for (let k = -1; k < 2; k++) {
+        for (let f = -1; f < 2; f++) {
+          if (i + k > -1 && j + f > -1 && i + k < matrix.length && j + f < matrix.length) {
+            if (matrix[i + k][j + f] == true) {
+              counter = counter + 1
+            }
+          }
+        }
+      }
+      if (matrix[i][j] == true) {
+        counter = counter - 1
+      }
+      matrixReworked[i][j] = counter
+    }
+  }
+  return matrixReworked
 }
 
 module.exports = {
