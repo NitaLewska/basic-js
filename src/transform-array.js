eca948acf89f3ed2a265f0a703ad56c129c4a884
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-25 02:31:37 +0300

    feat: transform-array and what-season ideas, half-done; 76/83 (02:01:48 Monday, December 25, 2023)

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,43 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+  if (arr.length = 0) {
+    return arr
+  }
+  let i = 0
+  while (arr[i]) {
+    if (arr[i] = '--discard-next') {
+      arr[i + 1] = 'DELETED'
+      i +=2
+    }
+    if (arr[i] = '--discard-prev') {
+      arr[i - 1] = ''
+      i +=2
+    }
+    if (arr[i] = '--double-next') {
+      arr.splice(i, 0, arr[i + 1])
+      i +=2
+    }
+    if (arr[i] = '--double-prev') {
+      arr[i - 1] = ''
+      i +=2
+    }
+  }
+  return arr.filter(a => a != 'DELETED')
 }
 
 module.exports = {
