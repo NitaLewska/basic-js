a504048badfcdf583367406494bfeee94c2c6b39
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-24 21:47:42 +0300

    feat: dns-stats; 55/83 (21:47:35 Sunday, December 24, 2023)

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  domains = domains.map(a => a.split('.').reverse())
+  let DNSstats = {}
+  for(let i=0; i<domains.length; i++) {
+    let domain = ''
+    for (let j=0; j<domains[i].length; j++){
+      domain = domain + '.' + domains[i][j]
+      DNSstats[domain] ? DNSstats[domain]++ : DNSstats[domain] = 1
+    }
+  }
+  return DNSstats
 }
 
 module.exports = {
